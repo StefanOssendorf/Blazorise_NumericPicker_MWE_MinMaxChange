d92b5eaeb378bd29dd1bbfe9d055c793b7410813
Stefan Ossendorf <stefan.ossendorf@googlemail.com>
2022-03-25 20:19:06 +0100

    MWE for min&max change created

---
+++ Blazorise-NumericPicker-MWE-MinMaxChange/FooContainer.cs
@@ -0,0 +1,13 @@
+﻿namespace Blazorise_NumericPicker_MWE_MinMaxChange; 
+
+public class FooContainer {
+    public ContainerKind Kind { get; set; }
+
+    public int Value { get; set; }
+}
+
+public enum ContainerKind {
+    Normal,
+    Special,
+    SuperSpecial
+}
---
+++ Blazorise-NumericPicker-MWE-MinMaxChange/Shared/FooContainerComponent.razor.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazorise_NumericPicker_MWE_MinMaxChange.Shared;
+
+public partial class FooContainerComponent {
+
+    [Parameter]
+    public FooContainer Data { get; set; }
+
+    public IList<ContainerKind> ContainerKinds { get; } = Enum.GetValues<ContainerKind>();
+
+    public int Minimum { get; set; }
+    public int Maximum { get; set; }
+
+    protected override void OnParametersSet() {
+        EnsureLimitsAreSet();
+        Console.WriteLine("Min & max set");
+        base.OnParametersSet();
+    }
+
+    private void EnsureLimitsAreSet() {
+        switch( Data.Kind ) {
+            case ContainerKind.Normal:
+                Minimum = 1;
+                Maximum = 5;
+                break;
+            case ContainerKind.Special:
+                Minimum = 10;
+                Maximum = 100;
+                break;
+            case ContainerKind.SuperSpecial:
+                Minimum = 6;
+                Maximum = 1000;
+                break;
+            default:
+                Minimum = Maximum = 0;
+                break;
+        }
+    }
+
+    private async Task KindChanged(ContainerKind kind) {
+        Data.Kind = kind;
+        EnsureLimitsAreSet();
+
+        //await InvokeAsync(StateHasChanged);
+    }
+}
